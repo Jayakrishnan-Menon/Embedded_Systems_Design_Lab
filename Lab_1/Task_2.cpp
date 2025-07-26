f5086bba5d8d5dbd18c3adc5e8ae7b460da208b3
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 02:51:23 +0530

    Update Task_2.cpp

--- Lab_1/Task_2.cpp
+++ Lab_1/Task_2.cpp
@@ -3,17 +3,9 @@ DigitalOut myled1(PC_4);
 DigitalOut myled2(PB_13);
 DigitalOut myled3(PB_14);
 DigitalOut myled4(PB_15);
-int main() {
-while(1) {
-myled1 = 1;
-myled2 = 0;
-myled3 = 1;
-myled4 = 0;
-wait(0.5);
-myled1 = 0;
-myled2 = 1;
-myled3 = 0;
-myled4 = 1;
-wait(0.5);
-}
+int main(){
+  while(1){
+    myled1 = 1; myled2 = 0; myled3 = 1; myled4 = 0; wait(0.5);
+    myled1 = 0; myled2 = 1; myled3 = 0; myled4 = 1; wait(0.5);
+  }
 }
