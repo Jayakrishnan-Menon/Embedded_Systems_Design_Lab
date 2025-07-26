7bdd9f1b63f80cdfac7da35255163075f12e42c6
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 04:52:44 +0530

    Update Task_1.cpp

--- Lab_2/Task_1.cpp
+++ Lab_2/Task_1.cpp
@@ -1,3 +1,6 @@
+//Lab Task-1: Write a C++ program with mbed APIs to display Hexadecimal Counting Pattern from 0 to 15 by blinking LEDs. 
+//Implement and verify this logic on the STM32 Nucleo-64 board using Keil Studio Cloud IDE.
+
 #include "mbed.h"
 BusOut myled(PC_4,PB_13,PB_14,PB_15);
 int i;
