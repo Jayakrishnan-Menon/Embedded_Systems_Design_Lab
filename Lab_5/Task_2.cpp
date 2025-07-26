a229cd673b982afc07084011e8367e24268f8dbe
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 05:01:07 +0530

    Update Task_2.cpp

--- Lab_5/Task_2.cpp
+++ Lab_5/Task_2.cpp
@@ -1,3 +1,6 @@
+//Lab Task-2: Write a program that accepts a serial input character via the “Teraterm” software from a host computer and displays it on the LCD. Assume LCD operates in 4-bit with EN
+//and RS active state. Design and verify this logic on Nucleo-152RE board using online Keil Studio platform.
+
 #include "mbed.h"
 #include "TextLCD.h"
 TextLCD lcd(PC_0,PC_1,PB_0,PA_4,PA_1,PA_0); // rs, e, d4-d7
