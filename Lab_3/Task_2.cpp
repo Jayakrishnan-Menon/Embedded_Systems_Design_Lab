dcb5ce3351dc89fd1d96134518be6e43cd9fc176
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 04:55:52 +0530

    Update Task_2.cpp

--- Lab_3/Task_2.cpp
+++ Lab_3/Task_2.cpp
@@ -1,3 +1,6 @@
+//Lab Task-2: Write a C++ program with mbed APIs to gradually increase and decrease LED brightness using PWM signal with variable duty cycle. 
+//Assume the LED is connected to PC_8 PWM pin, time period of the PWM signal as 10ms, increment/decrement duty cycle value is 10% and delay between each increment/decrement duty cycle is 0.5s.
+
 #include "mbed.h"
 PwmOut led(PC_8);
 int main(){
