1dcf6d679a7c67703ce0cbb70f6751b0eb235792
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 04:53:32 +0530

    Update Task_2.cpp

--- Lab_2/Task_2.cpp
+++ Lab_2/Task_2.cpp
@@ -1,3 +1,7 @@
+//Lab Task-2: Write a C++ program with mbed APIs to blink one LED at a time serially in a group of 4 LEDs using switch. 
+//If switch is LOW, blink the LEDs one at a time from left to right (1000, 0100, 0010, 0001 pattern) with 0.5 Sec delay between each.
+//If switch is HIGH, blink the LEDs one at a time from right to left (0001, 0010, 0100, 1000 pattern) with 0.5 Sec delay between each.
+
 #include "mbed.h"
 DigitalIn switch1(PC_13);
 BusOut myled(PC_4,PB_13,PB_14,PB_15);
