a5e6789dca47bfa95ec2e6250f0f924246773897
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 02:47:52 +0530

    Update Task_2.cpp

--- Lab_2/Task_2.cpp
+++ Lab_2/Task_2.cpp
@@ -1,27 +1,19 @@
 #include "mbed.h"
 DigitalIn switch1(PC_13);
 BusOut myled(PC_4,PB_13,PB_14,PB_15);
-int main() {
-while(1) {
-if(switch1==0){
-myled=8;
-wait(0.5);
-myled=4;
-wait(0.5);
-myled=2;
-wait(0.5);
-myled=1;
-wait(0.5);
-}
-else{
-myled=1;
-wait(0.5);
-myled=2;
-wait(0.5);
-myled=4;
-wait(0.5);
-myled=8;
-wait(0.5);
-}
-}
+int main(){
+  while(1){
+    if(switch1==0){
+      myled=8; wait(0.5);
+      myled=4; wait(0.5);
+      myled=2; wait(0.5);
+      myled=1; wait(0.5);
+    }
+    else{
+      myled=1; wait(0.5);
+      myled=2; wait(0.5);
+      myled=4; wait(0.5);
+      myled=8; wait(0.5);
+    }
+  }
 }
