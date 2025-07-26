9b85a21934959fc41171e3b1883e5acfa6e2e5de
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 04:57:56 +0530

    Update Task_1.cpp

--- Lab_4/Task_1.cpp
+++ Lab_4/Task_1.cpp
@@ -1,3 +1,6 @@
+//Lab Task-1: Write a C++ code with mbed APIs to receive a character (H) from PC via host terminal application (Tera term) & switch ON the on-board LED1. 
+//For all other character LED1 must be in OFF state. Implement and verify this logic on the STM32 Nucleo-64 board using Keil Studio Cloud IDE.
+
 #include "mbed.h"
 Serial pc(USBTX,USBRX);
 DigitalOut myled(PC_8);
