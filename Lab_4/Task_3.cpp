e2983b9ab747a512640da8c6d9d6f9fc0df28a06
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 03:09:46 +0530

    Create Task_3.cpp

---
+++ Lab_4/Task_3.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+Serial pc(USBTX,USBRX);
+AnalogIn ain(PC_3);
+PwmOut PWM1(PC_8);
+int main(){
+  while(1){
+    float value =ain*5;
+    pc.printf("The reading of pot:%f",value);
+    wait(1);
+    PWM1.period(0.01);
+    PWM1=ain;
+  }
+}
