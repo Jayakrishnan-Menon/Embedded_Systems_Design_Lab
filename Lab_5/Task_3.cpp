50a4065fdf4015e8a7322dd8a9c079cf86236055
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 05:01:40 +0530

    Update Task_3.cpp

--- Lab_5/Task_3.cpp
+++ Lab_5/Task_3.cpp
@@ -1,3 +1,6 @@
+//Lab Task-3: Write a program to display the potentiometer value with 4 decimal places on the LCD display on row-1 5th position. Assume LCD operates in 4 bit with EN and RS active state. 
+//Design and verify this logic on Nucleo-L152RE board using online Keil Studio Platform.
+
 #include "mbed.h"
 #include "TextLCD.h"
 AnalogIn ain(PC_5);
