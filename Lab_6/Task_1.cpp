a6f20988d89cfb41f56c71aacb5241aa5180d772
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 05:02:25 +0530

    Update Task_1.cpp

--- Lab_6/Task_1.cpp
+++ Lab_6/Task_1.cpp
@@ -1,3 +1,6 @@
+//Lab Task-1: Write a program to control the servo motor by rotating slowly from 0 degrees to 180 degrees, 45 degree at a time. When the motor has to be rotated 180 degrees, it will
+//return to the initial position. Implement and verify this logic on STM32 board.
+
 #include "mbed.h"
 PwmOut PWM1(PC_8);
 int main(){
