e7e86077438cff53945e31f12e4de0bc329ee0e5
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 04:16:16 +0530

    Create Task_1_Master.cpp

---
+++ Lab_B/Task_1_Master.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+SPI spi(PB_15, PB_14, PB_13);
+DigitalOut cs(PB_12);
+Serial pc(USBTX, USBRX);
+int main(){
+  char send_val;
+  pc.printf("Press any key to start...\n");
+  while (1){
+    send_val = pc.getc();
+    pc.printf("%c", send_val);
+    cs = 0;
+    spi.write(send_val);
+    cs = 1;
+    wait(0.01);
+  }
+}
