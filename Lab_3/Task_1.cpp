0295f456975be669f32f2650f519b5faed0e92ae
Jayakrishnan Menon <jkmenon18@gmail.com>
2025-07-27 04:55:01 +0530

    Update Task_1.cpp

--- Lab_3/Task_1.cpp
+++ Lab_3/Task_1.cpp
@@ -1,3 +1,6 @@
+//Lab Task-1: Write a C++ program with mbed APIs to control the brightness of the LED using a PWM signal with duty cycle 75%. Assume PWM period as 2 Seconds. 
+//Implement and verify this logic on the STM32 Nucleo-64 board using Keil Studio Cloud IDE.
+
 #include "mbed.h"
 PwmOut led(PC_8);
 int main(){
